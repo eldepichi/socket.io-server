9c28bfb43966de7008a770120093cb30b1d6ba9f
jerfarias <jerfarias@uveg.edu.mx>
2023-08-07 11:06:27 -0600

    Cambios sobre bandas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,7 +1,19 @@
 const { io } = require('../index')
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Breaking Benjamin'));
+bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('Héroes del silencio'));
+bands.addBand(new Band('Mellalica'));
 
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
+
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
     });
@@ -9,6 +21,21 @@ io.on('connection', client => {
     client.on('mensaje', (payload) => {
         console.log("Mensaje!!", payload);
         io.emit('mensaje', { mensaje: 'Nuevo Mensaje' });
-    })
+    });
+
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload) => {
+        bands.addBand(new Band(payload.name))
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id)
+        io.emit('active-bands', bands.getBands());
+    });
 
 });
